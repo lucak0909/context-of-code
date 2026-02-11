47059113a0334ae19671871bb9cd7a3fee3590bd
Luca Knierim <lucak0909@gmail.com>
2026-02-11 13:41:15 +0000

    add sql table dataclasses

---
+++ src/database/db_dataclasses.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+from typing import Optional
+
+
+@dataclass
+class Device:
+    id: UUID
+    user_id: UUID
+    name: str
+    device_type: str
+    created_at: datetime
+
+
+@dataclass
+class Room:
+    id: UUID
+    user_id: UUID
+    name: str
+    created_at: datetime
+
+
+@dataclass
+class Sample:
+    id: int
+    device_id: UUID
+    room_id: UUID
+    ts: datetime
+    wifi_rssi_dbm: float
+    link_speed_mbps: Optional[float]
+    is_connected: Optional[bool]
+    created_at: datetime
