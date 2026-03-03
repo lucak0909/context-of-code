8796978f7c03f587a7a7a115418909667b2b7725
conorclancy <conorclancy05@gmail.com>
2026-03-03 14:01:25 +0000

    added reporting blueprint

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from datetime import datetime, timezone
 from web_app.blueprints.api import api_bp
+from web_app.blueprints.reporting import reporting_bp
 
 app = Flask(__name__)
+CORS(app)
 
 # Register Blueprints
 app.register_blueprint(api_bp, url_prefix="/api")
+app.register_blueprint(reporting_bp, url_prefix="/api/report")
 
 @app.route('/health', methods=['GET'])
 def health():
